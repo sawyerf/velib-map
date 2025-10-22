26c701ec6b274f06582d49867d99d97b226d73bd
Sawyerf <sawyer.flink@protonmail.ch>
2025-10-22 22:03:00 +0200

    Add: tuto

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 // App.jsx
 import React from "react";
-import Map from "./components/map";
-import StatInfo from "./components/StatInfo";
-import Heatmap from "./components/Heatmap";
-import Station from "./components/Station";
-import { BarChart } from '@mui/x-charts/BarChart';
-import stat from "./libs/stats";
 
 import "./styles/App.css";
+import Stats from "./components/Stats";
 
 function App() {
-  const [points, setPoints] = React.useState([]);
-  const walletOperations = React.useRef([]);
-  const [stats, setStats] = React.useState({});
-  const [statsByDay, setStatsByDay] = React.useState({});
-  const [statsByHour, setStatsByHour] = React.useState([]);
+  const [walletOperations, setWalletOperations] = React.useState([]);
   const [typeVelib, setTypeVelib] = React.useState("ALL");
-  const [topStations, setTopStations] = React.useState([]);
-
-  const updateStats = React.useCallback(() => {
-    const walletOpsFiltered = stat.filterOpe(walletOperations.current, typeVelib);
-    setPoints(stat.processPoints(walletOpsFiltered));
-    setStats(stat.getStat(walletOpsFiltered));
-    setStatsByDay(stat.getStatsByDay(walletOpsFiltered));
-    setStatsByHour(stat.getStatsByHour(walletOpsFiltered));
-    setTopStations(stat.getTopStations(walletOpsFiltered, 5));
-  }, [typeVelib]);
-
-  React.useEffect(() => {
-    updateStats();
-  }, [updateStats]);
 
   document.body.ondrop = (event) => {
     handleFileChange(event);
@@ -54,8 +31,7 @@ function App() {
     reader.onload = (e) => {
       try {
         const json = JSON.parse(e.target.result);
-        walletOperations.current = json.walletOperations.filter(op => op.parameter3.DISTANCE !== '0.0');
-        updateStats();
+        setWalletOperations(json.walletOperations.filter(op => op.parameter3.DISTANCE !== '0.0'));
       } catch (error) {
         console.error("Invalid JSON file", error);
       }
@@ -64,11 +40,21 @@ function App() {
   }
 
   return (
-    <div className="main-container">
+    <div className="main-container" style={{ justifyContent: "center" }}>
       <h1>Statistiques Vélib</h1>
+      {
+        walletOperations.length === 0 && (
+          <div className="tuto">
+            <p><a href="https://www.velib-metropole.fr/login" target="_blank" rel="noreferrer">Connectez-vous</a> à votre compte Vélib</p>
+            <p>Cliquez sur "Télecharger vos trajets" en dessous</p>
+            <p>Enregistrez le fichier JSON.</p>
+            <p>Importez le fichier.</p>
+          </div>
+        )
+      }
       <div style={{
         display: "flex",
-        flexDirection: "wrap",
+        flexWrap: "wrap",
         gap: "16px",
         alignItems: "center",
         justifyContent: "center",
@@ -76,57 +62,18 @@ function App() {
         marginTop: "10px",
       }}
       >
-        <a href="https://www.velib-metropole.fr/api/private/getCourseList?limit=10000" target="_blank" rel="noreferrer">Télécharger vos trajets (JSON)</a>
+        <a className="download" href="https://www.velib-metropole.fr/api/private/getCourseList?limit=100000" target="_blank" rel="noreferrer">Télécharger vos trajets (JSON)</a>
         <input type="file" accept=".json" onChange={handleFileChange} />
       </div>
-      <div class="buttons">
-        <button id="all" onClick={() => setTypeVelib("ALL")} disabled={typeVelib === "ALL"}>Tout</button>
-        <button id="mechanical" onClick={() => setTypeVelib("MECHANICAL")} disabled={typeVelib === "MECHANICAL"}>Mécanique</button>
-        <button id="electric" onClick={() => setTypeVelib("ELECTRIC")} disabled={typeVelib === "ELECTRIC"}>Électrique</button>
-      </div>
-      <Map points={points} />
       {
-        points.length > 0 && (
-          <>
-            <div style={{ display: "grid", gap: "16px", marginTop: "20px", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", width: "100%", maxWidth: "800px" }}>
-              <StatInfo name="Nombre de trajets" value={stats.totalRides} unit="" />
-              <StatInfo name="Vitesse maximale" value={stats.maxSpeed?.toFixed(2)} unit="km/h" />
-              <StatInfo name="Vitesse moyenne" value={stats.avgSpeed} unit="km/h" />
-              <StatInfo name="Distance moyenne" value={stats.avgDistance?.toFixed(2)} unit="km" />
-              <StatInfo name="Distance maximale" value={stats.maxDistance?.toFixed(2)} unit="km" />
-              <StatInfo name="Distance totale" value={stats.totalDistance?.toFixed(2)} unit="km" />
-              <StatInfo name="Durée moyenne" value={stats.avgDuration?.toFixed(2)} unit="min" />
-              <StatInfo name="Durée totale" value={stats.totalDuration?.toFixed(2)} unit="min" />
-              <StatInfo name="Stations utilisées" value={points.length} unit="" />
-            </div>
-            <h3 className="subtitle">Top 5 des stations les plus utilisées</h3>
-            <div className="stations-container">
-              {topStations.map((station) => (
-                <Station key={station.id} name={station.name} hits={station.hits} />
-              ))}
-            </div>
-            <h3 className="subtitle">Nombre de trajets par jour</h3>
-            <Heatmap data={statsByDay} />
-          </>
-        )}
-      {
-        statsByHour.length > 0 && (
-          <>
-            <h3 className="subtitle">Nombre de trajets par heure de la journée</h3>
-            <BarChart
-              dataset={statsByHour}
-              xAxis={[{ dataKey: 'hour', label: 'Heure de la journée' }]}
-              series={[
-                { dataKey: 'count', label: 'Nombre de trajets', color: 'blue' },
-                // { dataKey: 'totalDistance', label: 'Distance totale (m)', color: 'green' },
-                // { dataKey: 'totalDuration', label: 'Durée totale (s)', color: 'orange' },
-              ]}
-              width={Math.min(window.innerWidth - 40, 800)}
-              height={Math.min(window.innerWidth - 40, 800) * 0.5}
-            />
-          </>
-        )
+        walletOperations?.length > 0 &&
+        <div className="buttons">
+          <button id="all" onClick={() => setTypeVelib("ALL")} disabled={typeVelib === "ALL"}>Tout</button>
+          <button id="mechanical" onClick={() => setTypeVelib("MECHANICAL")} disabled={typeVelib === "MECHANICAL"}>Mécanique</button>
+          <button id="electric" onClick={() => setTypeVelib("ELECTRIC")} disabled={typeVelib === "ELECTRIC"}>Électrique</button>
+        </div>
       }
+      <Stats walletOperations={walletOperations} typeVelib={typeVelib} />
     </div>
   );
 }
---
+++ src/components/Stats.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+import stat from "../libs/stats";
+import Map from "../components/map";
+import StatInfo from "../components/StatInfo";
+import Heatmap from "../components/Heatmap";
+import Station from "../components/Station";
+import { BarChart } from '@mui/x-charts/BarChart';
+
+const Stats = ({ walletOperations, typeVelib }) => {
+  const [points, setPoints] = React.useState([]);
+  const [stats, setStats] = React.useState(null);
+  const [statsByDay, setStatsByDay] = React.useState(null);
+  const [statsByHour, setStatsByHour] = React.useState([]);
+  const [topStations, setTopStations] = React.useState([]);
+
+  const updateStats = React.useCallback(() => {
+    const walletOpsFiltered = stat.filterOpe(walletOperations, typeVelib);
+    setPoints(stat.processPoints(walletOpsFiltered));
+    setStats(stat.getStat(walletOpsFiltered));
+    setStatsByDay(stat.getStatsByDay(walletOpsFiltered));
+    setStatsByHour(stat.getStatsByHour(walletOpsFiltered));
+    setTopStations(stat.getTopStations(walletOpsFiltered, 5));
+  }, [walletOperations, typeVelib]);
+
+  React.useEffect(() => {
+    console.log("Updating stats...");
+    console.log("Wallet operations:", walletOperations);
+    updateStats();
+  }, [walletOperations, typeVelib]);
+
+  if (!walletOperations?.length) return null;
+  return (
+    <>
+      <Map points={points} />
+      {stats && (
+        <div style={{ display: "grid", gap: "16px", marginTop: "20px", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", width: "100%", maxWidth: "800px" }}>
+          <StatInfo name="Nombre de trajets" value={stats.totalRides} unit="" />
+          <StatInfo name="Vitesse maximale" value={stats.maxSpeed?.toFixed(2)} unit="km/h" />
+          <StatInfo name="Vitesse moyenne" value={stats.avgSpeed} unit="km/h" />
+          <StatInfo name="Distance moyenne" value={stats.avgDistance?.toFixed(2)} unit="km" />
+          <StatInfo name="Distance maximale" value={stats.maxDistance?.toFixed(2)} unit="km" />
+          <StatInfo name="Distance totale" value={stats.totalDistance?.toFixed(2)} unit="km" />
+          <StatInfo name="Durée moyenne" value={stats.avgDuration?.toFixed(2)} unit="min" />
+          <StatInfo name="Durée totale" value={stats.totalDuration?.toFixed(2)} unit="min" />
+          <StatInfo name="Stations utilisées" value={points.length} unit="" />
+        </div>
+      )}
+      {topStations.length > 0 && (
+        <>
+          <h3 className="subtitle">Top 5 des stations les plus utilisées</h3>
+          <div className="stations-container">
+            {topStations.map((station) => (
+              <Station key={station.id} name={station.name} hits={station.hits} />
+            ))}
+          </div>
+        </>
+      )}
+      {statsByDay && (
+        <>
+          <h3 className="subtitle">Nombre de trajets par jour</h3>
+          <Heatmap data={statsByDay} />
+        </>
+      )}
+      {
+        statsByHour.length > 0 && (
+          <>
+            <h3 className="subtitle">Nombre de trajets par heure de la journée</h3>
+            <BarChart
+              dataset={statsByHour}
+              xAxis={[{ dataKey: 'hour', label: 'Heure de la journée' }]}
+              series={[
+                { dataKey: 'count', label: 'Nombre de trajets', color: 'blue' },
+                // { dataKey: 'totalDistance', label: 'Distance totale (m)', color: 'green' },
+                // { dataKey: 'totalDuration', label: 'Durée totale (s)', color: 'orange' },
+              ]}
+              width={Math.min(window.innerWidth - 40, 800)}
+              height={Math.min(window.innerWidth - 40, 800) * 0.5}
+            />
+          </>
+        )
+      }
+    </>
+  )
+}
+
+export default Stats;
